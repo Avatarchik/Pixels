5e71ad59cb7bc47a634f2b76470ad3cbd945741e
jamessmarion <jamessmarion@gmail.com>
2015-08-03 13:14:51 -0400

    4-7 half done

--- Assets/Scripts/Levels/04. GameDev/DoomShoot.js
+++ Assets/Scripts/Levels/04. GameDev/DoomShoot.js
@@ -11,11 +11,50 @@ var colorForChange:Color;
 @HideInInspector var length:float;
 @HideInInspector var timer:float;
 
+@HideInInspector var leftLaneLocation:float;
+@HideInInspector var centerLaneLocation:float;
+@HideInInspector var rightLaneLocation:float;
+
+@HideInInspector var leftLaneHeights:float[];
+@HideInInspector var centerLaneHeights:float[];
+@HideInInspector var rightLaneHeights:float[];
+
+var normalEnemy:GameObject;
+var hardEnemy:GameObject;
+
+var normalEnemySprites:Sprite[];
+var hardEnemySprites:Sprite[];
+
+@HideInInspector var leftEnemy:GameObject;
+@HideInInspector var centerEnemy:GameObject;
+@HideInInspector var rightEnemy:GameObject;
+
+@HideInInspector var leftEnemyPosition:int;
+@HideInInspector var centerEnemyPosition:int;
+@HideInInspector var rightEnemyPosition:int;
+
+@HideInInspector var pauseTime:float;
+@HideInInspector var movementWaitTime:float;
+
 function Start () {
 	// Basic world variable initialization.
 	importantFinger = -1;
 	
 	// Level specific variable initialization.
+	leftLaneLocation = -6;
+	centerLaneLocation = 0;
+	rightLaneLocation = 6;
+	leftLaneHeights = new float[5];
+	centerLaneHeights = new float[5];
+	rightLaneHeights = new float[5];
+	
+	leftLaneHeights = [-.5,-.85,-1.25,-1.65,-2];
+	centerLaneHeights = [2,1.0,0,-1,-2];
+	rightLaneHeights = leftLaneHeights;
+	
+	leftEnemyPosition = 0;
+	centerEnemyPosition = 0;
+	rightEnemyPosition = 0;
 	
 	// Speed and difficulty information.
 	if(Application.loadedLevelName == "MicroTester")
@@ -33,6 +72,10 @@ function Start () {
 	timer = length;
 	UITimer.currentTarget = length;
 	UITimer.counter = 0;
+	
+	pauseTime = .3;
+	movementWaitTime = .3;
+	
 	// If the color of the UI should change.
 	if(colorChange)
 	{
@@ -72,7 +115,175 @@ function Update () {
 }
 
 function Play () {
+	var lane0NotDone:boolean = true;
+	var lane1NotDone:boolean = true;
+	var lane2NotDone:boolean = true;
+	while(lane0NotDone || lane1NotDone || lane2NotDone)
+	{
+		switch(Random.Range(0,3))
+		{
+			case 0:
+				if(lane0NotDone)
+				{
+					lane0NotDone = false;
+					Lane(0);
+				}
+				break;
+			case 1:
+				if(lane1NotDone)
+				{
+					lane1NotDone = false;
+					Lane(1);
+				}
+				break;
+			case 2:
+				if(lane2NotDone)
+				{
+					lane2NotDone = false;
+					Lane(2);
+				}
+				break;
+			default:
+				break;
+		}
+		yield WaitForSeconds(Random.Range(pauseTime*.6,pauseTime*1.4));
+	}
+}
 
+function Lane (lane:int) {
+	var counter:float;
+	var sprites:Sprite[];
+	var toCreate:GameObject;
+	while(true)
+	{
+		switch(lane)
+		{
+			case 0:
+				if(leftEnemy == null)
+				{
+					yield WaitForSeconds(Random.Range(pauseTime * .6, pauseTime * 1.4));
+					if(difficulty == 3 && Random.Range(0,2) == 1)
+					{
+						toCreate = hardEnemy;
+						sprites = hardEnemySprites;
+					}
+					else
+					{
+						toCreate = normalEnemy;
+						sprites = normalEnemySprites;
+					}
+					leftEnemyPosition = 0;
+					leftEnemy = Instantiate(toCreate,Vector3(leftLaneLocation,leftLaneHeights[0],transform.position.z + 1),Quaternion.identity);
+					leftEnemy.transform.parent = transform;
+					counter = 0;
+				}
+				else
+				{
+					counter += Time.deltaTime;
+					if(counter > movementWaitTime)
+					{
+						if(leftEnemyPosition < 4)
+						{
+							leftEnemyPosition ++;
+							leftEnemy.transform.position.y = leftLaneHeights[leftEnemyPosition];
+							leftEnemy.GetComponent(SpriteRenderer).sprite = sprites[leftEnemyPosition];
+							counter = 0;
+						}
+						else
+						{
+							Destroy(leftEnemy);
+							HurtPlayer();
+						}
+					}
+				}
+				break;
+			case 1:
+				if(centerEnemy == null)
+				{
+					yield WaitForSeconds(Random.Range(pauseTime * .6, pauseTime * 1.4));
+					if(difficulty == 3 && Random.Range(0,2) == 1)
+					{
+						toCreate = hardEnemy;
+						sprites = hardEnemySprites;
+					}
+					else
+					{
+						toCreate = normalEnemy;
+						sprites = normalEnemySprites;
+					}
+					centerEnemyPosition = 0;
+					centerEnemy = Instantiate(toCreate,Vector3(centerLaneLocation,centerLaneHeights[0],transform.position.z+1),Quaternion.identity);
+					centerEnemy.transform.parent = transform;
+					counter = 0;
+				}
+				else
+				{
+					counter += Time.deltaTime;
+					if(counter > movementWaitTime)
+					{
+						if(centerEnemyPosition < 4)
+						{
+							centerEnemyPosition ++;
+							centerEnemy.transform.position.y = centerLaneHeights[centerEnemyPosition];
+							centerEnemy.GetComponent(SpriteRenderer).sprite = sprites[centerEnemyPosition];
+							counter = 0;
+						}
+						else
+						{
+							Destroy(centerEnemy);
+							HurtPlayer();
+						}
+					}
+				}
+				break;
+			case 2:
+				if(rightEnemy == null)
+				{
+					yield WaitForSeconds(Random.Range(pauseTime * .6, pauseTime * 1.4));
+					if(difficulty == 3 && Random.Range(0,2) == 1)
+					{
+						toCreate = hardEnemy;
+						sprites = hardEnemySprites;
+					}
+					else
+					{
+						toCreate = normalEnemy;
+						sprites = normalEnemySprites;
+					}
+					rightEnemyPosition = 0;
+					rightEnemy = Instantiate(toCreate,Vector3(rightLaneLocation,rightLaneHeights[0],transform.position.z+1),Quaternion.identity);
+					rightEnemy.transform.parent = transform;
+					counter = 0;
+				}
+				else
+				{
+					counter += Time.deltaTime;
+					if(counter > movementWaitTime)
+					{
+						if(rightEnemyPosition < 4)
+						{
+							rightEnemyPosition ++;
+							rightEnemy.transform.position.y = rightLaneHeights[rightEnemyPosition];
+							rightEnemy.GetComponent(SpriteRenderer).sprite = sprites[rightEnemyPosition];
+							counter = 0;
+						}
+						else
+						{
+							Destroy(rightEnemy);
+							HurtPlayer();
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		yield;
+	}
+}
+
+function HurtPlayer () {
+	Debug.Log("hey: " + Time.time);
 }
 
 function Finish(completionStatus:boolean,waitTime:float) {
