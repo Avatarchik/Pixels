838669b76158b797f8c8e75e994e3e1788d9761b
jamessmarion <jamessmarion@gmail.com>
2015-08-26 18:13:17 -0400

    petum
    panum

--- Assets/Scripts/Levels/05. Neverland/QueueLine.js
+++ Assets/Scripts/Levels/05. Neverland/QueueLine.js
@@ -15,11 +15,18 @@ var player:GameObject;
 
 var people:GameObject[];
 
+@HideInInspector var movementSpeed:float;
+@HideInInspector var newSpeed:float;
+@HideInInspector var distanceTouch:float;
+@HideInInspector var playerDistance:float;
+
 function Start () {
 	// Basic world variable initialization.
 	importantFinger = -1;
 	
 	// Level specific variable initialization.
+	distanceTouch = 2;
+	playerDistance = 4;
 	
 	// Speed and difficulty information.
 	if(Application.loadedLevelName == "MicroTester")
@@ -32,20 +39,34 @@ function Start () {
 		speed = GameManager.speed;
 		difficulty = GameManager.difficulty;
 	}
-	length = 3 + 5/speed;
+	length = 5;
 	timer = length;
 	UITimer.currentTarget = length;
 	UITimer.counter = 0;
+	movementSpeed = 3;
+	
 	// If the color of the UI should change.
 	if(colorChange)
 	{
 		StartCoroutine(ColorChange());
 	}
 	// If The game doesn't just run in Update.
+	SpeedStuff();
 	Play();
 }
 
+function SpeedStuff() {
+	var initial:float = movementSpeed;
+	while(true)
+	{
+		yield WaitForSeconds(.5);
+		newSpeed = Random.Range(initial * .5, initial * 1.5);
+		yield;
+	}
+}
+
 function Update () {
+	movementSpeed = Mathf.MoveTowards(movementSpeed,newSpeed,Time.deltaTime * 2);
 	timer -= Time.deltaTime;
 	if(timer < 0 && !finished)
 	{
@@ -63,10 +84,22 @@ function Update () {
 			}
 		}
 	}
+	player.GetComponent(PlayerManager).currentState = PlayerState.StandingFront;
 	// If that finger still exists and the game isn't paused, do stuff. (Always fires when finger is first touched.)
 	if(Finger.GetExists(importantFinger) && !Master.paused)
 	{
-		
+		if(Mathf.Abs(Finger.GetPosition(importantFinger).x-player.transform.position.x) < playerDistance)
+		{
+			player.transform.position.x = Mathf.MoveTowards(player.transform.position.x, Finger.GetPosition(importantFinger).x, Time.deltaTime * 5);
+			if(Finger.GetPosition(importantFinger).x > player.transform.position.x && Mathf.Abs(Finger.GetPosition(importantFinger).x - player.transform.position.x) > 0)
+			{
+				player.GetComponent(PlayerManager).currentState = PlayerState.WalkingRight;
+			}
+			else if(Finger.GetPosition(importantFinger).x < player.transform.position.x && Mathf.Abs(Finger.GetPosition(importantFinger).x - player.transform.position.x) > 0)
+			{
+				player.GetComponent(PlayerManager).currentState = PlayerState.WalkingLeft;
+			}
+		}
 	}
 	else if(!Finger.GetExists(importantFinger))
 	{
@@ -75,7 +108,38 @@ function Update () {
 }
 
 function Play () {
-
+	yield WaitForSeconds(.5);
+	if(difficulty == 3)
+	{	
+		var counter:float = Random.Range(0,1.5);
+		while(true)
+		{
+			if(counter > 0)
+			{
+				for(var i:int = 0; i < people.length; i++)
+				{
+					people[i].transform.position.x += Time.deltaTime * movementSpeed;
+				}
+				counter -= Time.deltaTime;
+			}
+			else if (counter < -1)
+			{
+				counter = Random.Range(.5,2.5);
+			}
+			yield;
+		}
+	}
+	else
+	{
+		while(true)
+		{
+			for(i = 0; i < people.length; i++)
+			{
+				people[i].transform.position.x += Time.deltaTime * movementSpeed;
+			}
+			yield;
+		}
+	}
 }
 
 function Finish(completionStatus:boolean,waitTime:float) {
