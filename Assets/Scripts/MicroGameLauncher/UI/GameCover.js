f961deabe0967a61b6fb9c59af409d064a76d7a8
jamessmarion <jamessmarion@gmail.com>
2015-10-10 16:52:49 -0400

    remix
    updates

--- Assets/Scripts/MicroGameLauncher/UI/GameCover.js
+++ Assets/Scripts/MicroGameLauncher/UI/GameCover.js
@@ -9,6 +9,40 @@ var failureSprite:Sprite;
 
 var displayObject:SpriteRenderer;
 
+var randomize:boolean = false;;
+
+function Awake () {
+	if(randomize)
+	{
+		origin = transform.position;
+		destination.z = transform.position.z;
+		if(Random.value < .5)
+		{
+			destination.x = Random.Range(-9,9);
+			if(Random.value < .5)
+			{
+				destination.y = 20;
+			}
+			else
+			{
+				destination.y = -20;
+			}
+		}
+		else
+		{
+			destination.y = Random.Range(-9,9);
+			if(Random.value < .5)
+			{
+				destination.x = 20;
+			}
+			else
+			{
+				destination.x = -20;
+			}
+		}
+	}
+}
+
 function DisplayChange (status:String) { 
 	if(displayObject != null)
 	{
