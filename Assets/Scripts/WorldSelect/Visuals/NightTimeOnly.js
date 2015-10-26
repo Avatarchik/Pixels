db83ea4a3362c5973252a31a5f69122c1634f34b
jamessmarion <jamessmarion@gmail.com>
2015-10-26 01:32:48 -0400

    tiny changes
    woo

--- Assets/Scripts/WorldSelect/Visuals/NightTimeOnly.js
+++ Assets/Scripts/WorldSelect/Visuals/NightTimeOnly.js
@@ -2,16 +2,21 @@
 
 @HideInInspector var sprite:SpriteRenderer;
 
+var worldVar:String;
+
 function Start () {
 	sprite = GetComponent(SpriteRenderer);
 	if(TimeManager.state == TimeState.NightNormal || TimeManager.state == TimeState.NightEvil)
 	{
-		//sprite.enabled = true;
 	}
 	else
 	{
 		sprite.enabled = false;
 	}
+	if(worldVar != "" && PlayerPrefs.GetInt(worldVar) != 1)
+	{
+		sprite.enabled = false;
+	}	
 }
 
 function Update () {
