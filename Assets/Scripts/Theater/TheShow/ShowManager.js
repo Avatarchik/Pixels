5793ff017f44696b00d0ada50392d98036d9c892
jamessmarion <jamessmarion@gmail.com>
2015-11-24 19:36:43 -0500

    uh
    uh

--- Assets/Scripts/Theater/TheShow/ShowManager.js
+++ Assets/Scripts/Theater/TheShow/ShowManager.js
@@ -10,9 +10,18 @@ function Update () {
 
 class Scene {
 	var gameName:String;
-	var variableCheck:String;
+	var gameEndTime:float;
+	var effects:SceneEffects;
+	var info:SceneInfo;
+}
+
+class SceneEffects {
 	var movingObjects:GameObject[];
 	var colorChangeObjects:SpriteRenderer[];
 	var newColor:Color[];
+}
+
+class SceneInfo {
+	var variableCheck:String;
 	var maximumScore:float;
 }
