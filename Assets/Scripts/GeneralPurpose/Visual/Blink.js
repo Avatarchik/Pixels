121eb28b6293213c5882ed2b866814f353e11784
jamessmarion <jamessmarion@gmail.com>
2016-01-18 04:42:43 -0500

    so much work yo
    wow

---
+++ Assets/Scripts/GeneralPurpose/Visual/Blink.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+	Blink();
+}
+
+function Update () {
+
+}
+
+function Blink () {
+	while(true)
+	{
+		yield WaitForSeconds(Random.Range(.6,7.5));
+		GetComponent(SpriteRenderer).color.a = 1;
+		yield WaitForSeconds(Random.Range(.07,.3));
+		GetComponent(SpriteRenderer).color.a = 0;
+		yield;
+	}
+}
---
+++ Assets/Scripts/Theater/PaidSongs/RotateSetPiece.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var amount:float;
+var stop:boolean;
+var stopAmount:float;
+
+function Start () {
+	if(stop)
+	{
+		LimitedRotateStuff();
+	}
+	else
+	{
+		RotateStuff();
+	}
+}
+
+function Update () {
+}
+
+function RotateStuff () {
+	while(true)
+	{
+		transform.Rotate(Vector3(0,0,Time.deltaTime * amount));
+		yield;
+	}
+}
+
+function LimitedRotateStuff () {
+	var soFar:float = 0;
+	while(Mathf.Abs(soFar) < stopAmount)
+	{
+		transform.Rotate(Vector3(0,0,Time.deltaTime * amount));
+		soFar += Time.deltaTime * amount;
+		yield;
+	}
+}
